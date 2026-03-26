2b9bea6a30bb06a1edd6ac0b1b1ffb75ab3c7e0c
EMMAN LIM <EMMAN_LIM_98@YAHOO.COM>
2026-03-26 15:28:41 +0400

    Fixing Homepage.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,5 +1,5 @@
 import AboutSevenSpikes from '@/components/sections/about-seven-spikes';
-import BenefitsGrid from '@/components/sections/expertise';
+import ExpertiseGrid from '@/components/sections/expertise';
 import TestimonialsSection from '@/components/sections/client-testimonial';
 import FaqAccordion from '@/components/sections/faq-accordion';
 import HeroSection from '@/components/sections/hero-section';
@@ -12,10 +12,7 @@ export default async function Home() {
     <>
       <HeroSection />
       <AboutSevenSpikes />
-      <BenefitsGrid />
-      <TestimonialsSection />
-      <PricingSection />
-      <FaqAccordion />
+      <ExpertiseGrid />
     </>
   );
 }
