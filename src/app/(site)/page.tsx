4fd21926fa750c56db3cb897bc14cd2a6c4342cb
EMMAN LIM <EMMAN_LIM_98@YAHOO.COM>
2026-03-26 15:31:01 +0400

    Fixing bugs.

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,11 +1,6 @@
 import AboutSevenSpikes from '@/components/sections/about-seven-spikes';
 import ExpertiseGrid from '@/components/sections/expertise';
-import TestimonialsSection from '@/components/sections/client-testimonial';
-import FaqAccordion from '@/components/sections/faq-accordion';
 import HeroSection from '@/components/sections/hero-section';
-import ToolsTab from '@/components/sections/tools-tab';
-import { CoreFeatures } from '@/components/sections/core-features';
-import PricingSection from '@/components/sections/pricing';
 
 export default async function Home() {
   return (
